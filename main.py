983f34d4ccc50006a1b33de8507eaaeae743cf3b
ohsr <mx.france80@protonmail.com>
2020-04-29 21:57:59 +0200

    New Menu

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
+import argparse
 from firstDetection import launchDetection
 
 def main():
+    print("Reverse Finder - Recherche d'images sur Internet")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--cam", action="store_true", help="Envoi d'un fichier pour une recherhe sur internet")
+    parser.add_argument("-f", "--file", help="Envoi d'un fichier pour une recherhe sur internet")
 
-    choice ='0'
-    while choice =='0':
-        print("Reverse Finder - Recherche d'images sur Internet")
-        print("1- Utilisation de la webcam")
-        print("2- Envoi d'un fichier")
-
-        choice = input ("Que voulez-vous faire: ")
-
-        if choice == "1":
-            print("Ouverture WebCam")
-            webcam_menu()
-        elif choice == "2":
-            print("Envoi Fichier")
-        else:
-            print("Ce choix n'est pas possible")
+    args = parser.parse_args()
+    if args.cam:
+        webcam_menu()
+    elif args.file:
+        file_menu()
 
 def webcam_menu():
-    print("Menu Webcam ---")
+    print("-- Ouverture Webcam ---")
     launchDetection()
 
 def file_menu():
     print("Menu Fichier ---")
 
 
-main()
+if __name__ == "__main__":
+    main()
